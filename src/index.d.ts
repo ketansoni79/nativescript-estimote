47870b281f38cd3f68ee2ab29478372a481bc647
ketan <ketan@prioriti.com>
2018-03-20 18:53:04 +0530

    export native classes

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,6 +1,20 @@
 import { Common } from './estimote.common';
+export declare class ESTDeviceManagerDelegateImpl extends NSObject implements ESTDeviceManagerDelegate {
+  static ObjCProtocols: {
+      prototype: ESTDeviceManagerDelegate;
+  }[];
+  static new(): ESTDeviceManagerDelegateImpl;
+  deviceManagerDidDiscoverDevices(manager: any, devices: any): void;
+  deviceManagerDidFailDiscovery(manager: any): void;
+  deviceManagerDidFailDiscoveryWithError(manager: any, error: any): void;
+}
 export declare class Estimote extends Common {
-  // define your typings manually
-  // or..
-  // take the ios or android .d.ts files and copy/paste them here
+  message: string;
+  manager: ESTDeviceManager;
+  filter: ESTDeviceFilter;
+  delegate: ESTDeviceManagerDelegateImpl;
+  constructor();
+  startScanning(): void;
+  isScanning(): boolean;
+  stopScanning(): void;
 }
